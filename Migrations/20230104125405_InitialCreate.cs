b0daa0cb4f90c124607f2cdf68029f3e7cec0a87
Stephen Brown <stephen.brown@vontobel.com>
2023-01-04 14:37:17 +0100

    Initial commit

---
+++ Migrations/20230104125405_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SqliteNodaTimeLocalDateRepro.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestEntity",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TheDate = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestEntity", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestEntity");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NodaTime;
+
+using var db = new ReproContext();
+
+await db.Database.EnsureCreatedAsync();
+
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.Database.GetConnectionString()}.");
+
+// Create
+Console.WriteLine("Inserting a new entity");
+db.Add(new EntityWithLocalDate { TheDate = new LocalDate(2023, 1, 4) });
+db.SaveChanges();
+
+var resultsLocalDate = await db.TestEntity
+    .Select(b => new TestThingLocalDate
+    {
+        Date = b.TheDate
+    })
+    .ToListAsync();
+
+Console.WriteLine($"resultsLocalDate: {resultsLocalDate[0].Date}");
+
+var resultsDateTime = await db.TestEntity
+    .Select(b => new TestThingDateTime
+    {
+        Date = b.TheDate.ToDateTimeUnspecified()
+    })
+    .ToListAsync();
+
+Console.WriteLine($"resultsLocalDate: {resultsDateTime[0].Date}");
+
+public class ReproContext : DbContext
+{
+    public DbSet<EntityWithLocalDate> TestEntity { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source=repro_{SystemClock.Instance.GetCurrentInstant().ToUnixTimeTicks()}.db", c=> c.UseNodaTime());
+}
+
+public class EntityWithLocalDate
+{
+    public long Id { get; set; }
+    public LocalDate TheDate { get; set; }
+}
+
+public class TestThingDateTime
+{
+    public DateTime Date { get; set; }
+}
+
+public class TestThingLocalDate
+{
+    public LocalDate Date { get; set; }
+}
